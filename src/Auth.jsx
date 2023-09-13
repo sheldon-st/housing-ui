cc8ffc52e9035b2ef8ce02d5fd18fa02f11ca88a
sheldon-st <sheldon.st@northeastern.edu>
2023-09-13 16:11:54 -0400

    test

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { supabase } from './supabaseClient'
 
+//
 export default function Auth() {
   const [loading, setLoading] = useState(false)
   const [email, setEmail] = useState('')
